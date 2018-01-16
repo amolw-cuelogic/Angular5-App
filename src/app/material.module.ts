5f64bcf81f46d1dc5d49ba575bdb69ec3fcd6535
amolw-cuelogic <amol.wabale@cuelogic.com>
2018-01-16 17:55:37 +0530

    Initial Commit

---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+    imports: [MatButtonModule, MatMenuModule, MatIconModule, MatToolbarModule],
+    exports: [MatButtonModule, MatMenuModule, MatIconModule, MatToolbarModule],
+})
+export class MaterialModule { }
