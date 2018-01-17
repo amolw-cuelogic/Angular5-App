a8b718dcf6c67661b279c368c7d65f9774591fa1
amolw-cuelogic <amol.wabale@cuelogic.com>
2018-01-17 12:19:27 +0530

    Added log4net

--- Trunk/Source/3.Service Layer/Northwnd.Srv.Web.Api/Global.asax.cs
+++ Trunk/Source/3.Service Layer/Northwnd.Srv.Web.Api/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+//using log4net;
+using Northwnd.Utility.Log;
 
 namespace Northwnd.Srv.Web.Api
 {
@@ -18,6 +20,10 @@ namespace Northwnd.Srv.Web.Api
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
+            AppLogManager.Configure();
+            AppLogManager.GetLogger().Info("App Start");
+
         }
     }
 }
